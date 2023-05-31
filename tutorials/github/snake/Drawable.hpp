fce7daf9052c34101777faf5bec48bf27638cd62
Ky Bui Van <ky.buivan.dev@gmail.com>
2023-05-31 11:37:24 +0700

    Add game snake

---
+++ tutorials/github/snake/Drawable.hpp
@@ -0,0 +1,36 @@
+/*
+ * Snake game program using the SDL library
+ *
+ * @author J. Alvarez
+ */
+#include "Screen.hpp"
+#include <SDL.h>
+
+#ifndef DRAWABLE_HPP
+#define DRAWABLE_HPP
+
+/**
+ * This class represents an object that can be drawn on a Screen
+ */
+struct Drawable
+{
+    int m_x;
+    int m_y;
+
+    /**
+	 * Initialices Drawable object with a position
+	 * @param x X position of this object
+	 * @param y Y position of this object
+	 */
+    Drawable(int x, int y) : m_x(x), m_y(y)
+    {
+    }
+
+    /**
+	 * Draws on screen this object
+	 * @screen Reference to screen to draw on
+	 */
+    virtual void draw(Screen &screen) = 0;
+};
+
+#endif // DRAWABLE_HPP
---
+++ tutorials/github/snake/Food.hpp
@@ -0,0 +1,37 @@
+/*
+ * Snake game program using the SDL library
+ *
+ * @author J. Alvarez
+ */
+#include "Collideable.hpp"
+#include "Screen.hpp"
+#include <SDL.h>
+
+#ifndef FOOD_HPP
+#define FOOD_HPP
+
+/**
+ * This structure represents a food item
+ * Its (x,y) position corresponds to the top left pixel
+ */
+struct Food : Collideable
+{
+    static const Uint8 S_FOOD_RED;
+    static const Uint8 S_FOOD_GREEN;
+    static const Uint8 S_FOOD_BLUE;
+
+    static const unsigned int S_FOOD_WIDTH;
+    static const int S_VALUE;
+
+    /**
+	 * Default constructor for Food instances
+	 */
+    Food();
+
+    /**
+	 * @see Drawable#draw
+	 */
+    void draw(Screen &screen);
+};
+
+#endif // FOOD_HPP
